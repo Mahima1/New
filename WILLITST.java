9d4512cb4d1a7bc92314be894e5c1bb7c4af716d
Mahima Jain <mahimajain3110@gmail.com>
2019-07-15 13:46:09 +0530

    Updated

--- WILLITST.java
+++ WILLITST.java
@@ -3,17 +3,21 @@ import java.util.Scanner;
 public class WILLITST {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-//        String name = sc.nextLine();
-//        char gender = sc.next().charAt(0);
-//        int age = sc.nextInt();
-//        long mobileNo = sc.nextLong();
-//        double cgpa = sc.nextDouble();
-//        while (sc.hasNextInt()) {
-//            int n = sc.nextInt();
-//        }
         long num = sc.nextLong();
-        Double log2 = Math.log(num)/Math.log(2);
-        System.out.println(log2);
+
+        if (isPowerOfTwo(num)) {
+            System.out.println("TAK");
+        } else {
+            System.out.println("NIE");
+        }
+
+    }
+
+    static boolean isPowerOfTwo(long x) {
+
+        return x != 0 && ((x & (x - 1)) == 0);
     }
 }
+
+
+

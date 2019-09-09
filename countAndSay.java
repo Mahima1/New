9318775b21a6dc94558ff981c8dc82afb9224ef2
Mahima Jain <mahimajain3110@gmail.com>
2019-09-09 23:05:23 +0530

    COunt and say solution

--- countAndSay.java
+++ countAndSay.java
@@ -12,27 +12,38 @@ class countAndSay {
         for (int j = 2; j <= n; j++) {
             char current = a.charAt(0);
             newa="";
+            count=0;
+//            System.out.println("a len is : "+a.length());
             for (int i = 0; i < a.length(); i++) {
                 if (current != a.charAt(i)) {
                     newa = newa + (count) + (a.charAt(i - 1));
-                    System.out.println(newa);
-                    System.out.println("  count is "+count);
-                    count = 0;
+//                    System.out.println("newa is :" + newa);
+//                    System.out.println("  count is "+count);
+                    count = 1;
                     current = a.charAt(i);
                 } else {
                     count++;
                 }
             }
             char c = a.charAt(a.length()-1);
-            System.out.println(" c is "+c);
+//            System.out.println(" c is "+c);
+//            System.out.println("count after loop is "+count);
             newa = newa + (count) + (c);
             a=newa;
         }
         return a;
     }
     public static void main(String[] args){
-        int n=5;
-        System.out.println(cnt(n));
+
+        System.out.println(cnt(1));
+        System.out.println(cnt(2));
+        System.out.println(cnt(3));
+        System.out.println(cnt(4));
+        System.out.println(cnt(10));
+        System.out.println(cnt(90));
+        System.out.println(cnt(15));
+        System.out.println(cnt(23));
+        System.out.println(cnt(45));
 
     }
 }

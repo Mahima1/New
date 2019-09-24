49110f31217318afd7f9a6854ad8c401357a6ac4
Mahima Jain <mahimajain3110@gmail.com>
2019-09-25 00:31:45 +0530

    Anagram

--- anagram.java
+++ anagram.java
@@ -1,6 +1,9 @@
 class anagram {
     static boolean isAnagram(String s, String t) {
         long stemp = 0, ttemp = 0;
+        if (s.length() != t.length()) {
+            return false;
+        }
         for (int i = 0; i < s.length(); i++) {
             stemp += s.charAt(i);
             ttemp += t.charAt(i);
@@ -12,6 +15,6 @@ class anagram {
     }
 
     public static void main(String[] args) {
-        System.out.println(isAnagram("anagram", "naamgra"));
+        System.out.println(isAnagram("anagram", "Anagram"));
     }
 }
